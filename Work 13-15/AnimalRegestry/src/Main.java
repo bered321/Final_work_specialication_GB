37f7d817eb327e624321539b2068761389fbe662
bered321 <118922510+bered321@users.noreply.github.com>
2023-09-25 13:50:27 +0300

    correct some errors

--- Work 13-15/AnimalRegestry/src/Main.java
+++ Work 13-15/AnimalRegestry/src/Main.java
@@ -5,7 +5,7 @@ import View.AnimalView;
 
 public class Main {
     public static void main(String[] args) {
-        AnimalRegistry animalRegistry = new AnimalRegistry(); // Здесь создайте экземпляр вашей реализации AnimalRegistry
+        AnimalRegistry animalRegistry = new AnimalRegistry();
         AnimalPresenter animalPresenter = new AnimalPresenter(animalRegistry);
         AnimalView animalView = new AnimalView(animalPresenter);
 
